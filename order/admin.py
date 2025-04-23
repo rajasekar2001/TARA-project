e2e16aea9a38dad490415a711c9fe9d974c14339
rajasekar2001 <151813777+rajasekar2001@users.noreply.github.com>
2025-04-23 13:25:46 +0530

    Delete order directory

--- order/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Order
-
-admin.site.register(Order)
-# admin.site.register(PickOrder)
-# admin.site.register(PackOrder)
-# admin.site.register(Delivery)
-# admin.site.register(Invoice)
